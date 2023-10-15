7265ed80fd58fa35df4a6f99f57a76f9ffaabacc
theo <theotocar@gmail.com>
2023-10-14 21:33:11 -0300

    formularios

--- TorresCarmona.Theo/Trabajador.cs
+++ TorresCarmona.Theo/Trabajador.cs
@@ -1,7 +1,51 @@
-﻿namespace TorresCarmona.Theo
+﻿namespace Laburos
 {
-    public class Class1
+    public abstract class Trabajador
     {
+        public string nombre;
+        public string apellido;
+        public double salario;
+        public ETipo Tipo;
+
+        public Trabajador(string nombre, string apellido)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+
+        public Trabajador(string nombre, string apellido, double salario)
+            : this(nombre, apellido)
+        {
+            this.salario = salario;
+        }
+
+        public Trabajador(string nombre, string apellido, double salario, ETipo tipo)
+            : this(nombre, apellido, salario)
+        {
+            this.Tipo = tipo;
+        }
+
+        protected abstract void RealizarTarea();
+
+        public virtual string MostrarDatos()
+        {
+            return $"{nombre} {apellido} - {Tipo} - {salario}$";
+        }
+        public override string ToString()
+        {
+            return MostrarDatos();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Trabajador otroTrabajador = (Trabajador)obj;
+            return this.nombre == otroTrabajador.nombre && this.apellido == otroTrabajador.apellido;
+        }
 
     }
 }
