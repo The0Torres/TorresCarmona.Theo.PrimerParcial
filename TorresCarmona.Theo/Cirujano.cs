f11e619da9e6aa24ff60fb63050c412d282abb11
theo <theotocar@gmail.com>
2023-10-14 21:27:49 -0300

    clase cirujano

---
+++ TorresCarmona.Theo/Cirujano.cs
@@ -0,0 +1,61 @@
+﻿namespace Laburos
+{
+    public class Cirujano:Trabajador
+    {
+        public int cirugias;
+        public string especialidad;
+        public EHospitales Hospital;
+
+
+        public Cirujano(string nombre, string apellido, double salario,ETipo tipo, string especialidad)
+            : base(nombre, apellido, salario, tipo)
+        {
+            this.especialidad = especialidad;
+        }
+
+        public Cirujano(string nombre, string apellido, double salario,ETipo tipo ,string especialidad, EHospitales hospital)
+            : this(nombre, apellido , salario, tipo, especialidad)
+        {
+            this.Hospital = hospital;
+        }
+
+        public Cirujano(string nombre, string apellido, double salario,ETipo tipo ,string especialidad, EHospitales hospital, int cirugias)
+            : this(nombre, apellido, salario, tipo, especialidad, hospital)
+        {
+            this.cirugias = cirugias;
+        }
+
+        protected override void RealizarTarea()
+        {
+            Console.WriteLine("Realizando cirugia a un paciente");
+        }
+
+        public override string MostrarDatos()
+        {
+            return base.MostrarDatos() + $"Cirujano {especialidad} - Cirugías realizadas: {cirugias} - Hospital: {Hospital}";
+        }
+
+
+        public static bool operator ==(Cirujano cirujano1, Cirujano cirujano2)
+        {
+
+            return cirujano1.nombre == cirujano2.nombre && cirujano1.apellido == cirujano2.apellido;
+        }
+
+        public static bool operator !=(Cirujano cirujano1, Cirujano cirujano2)
+        {
+            return !(cirujano1 == cirujano2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Cirujano otroCirujano = (Cirujano)obj;
+            return this.nombre == otroCirujano.nombre && this.apellido == otroCirujano.apellido;
+        }
+    }
+}
