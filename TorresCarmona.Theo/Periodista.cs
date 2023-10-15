24b73585ca854adb6b9712d57d5b081a0e4cfaea
theo <theotocar@gmail.com>
2023-10-14 21:24:20 -0300

    clase periodista

---
+++ TorresCarmona.Theo/Periodista.cs
@@ -0,0 +1,62 @@
+﻿namespace Laburos
+{
+    public class Periodista : Trabajador
+    {
+        public double credibilidad;
+        public EMedios medio;
+        public string especializacion;
+
+        public Periodista(string nombre, string apellido, double salario, ETipo tipo, string especializacion) 
+            :base(nombre, apellido, salario, tipo)
+        {
+            this.especializacion = especializacion;
+        }
+
+        public Periodista(string nombre, string apellido, double salario, ETipo tipo,string especializacion, double credibilidad) 
+            :this(nombre, apellido, salario, tipo, especializacion)
+        {
+            this.credibilidad = credibilidad;
+        }
+
+        public Periodista(string nombre, string apellido, double salario, ETipo tipo, string especializacion, double credibilidad, EMedios medio) 
+            :this(nombre, apellido, salario, tipo, especializacion, credibilidad)
+        {
+            this.medio = medio;
+        }
+
+        protected override void RealizarTarea()
+        {
+            Console.WriteLine("Informandose de la actualidad");
+        }
+
+        public override string MostrarDatos()
+        {
+            return base.MostrarDatos() + $"Periodista {especializacion} - Medio: {medio} - Credibilidad: {credibilidad}%";
+        }
+
+
+        public static bool operator ==(Periodista periodista1, Periodista periodista2)
+        {
+
+            return periodista1.nombre == periodista2.nombre && periodista1.apellido == periodista2.apellido;
+        }
+
+        public static bool operator !=(Periodista periodista1, Periodista periodista2)
+        {
+            return !(periodista1 == periodista2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Periodista otroperiodista = (Periodista)obj;
+            return this.nombre == otroperiodista.nombre && this.apellido == otroperiodista.apellido;
+        }
+
+    }
+}
+
